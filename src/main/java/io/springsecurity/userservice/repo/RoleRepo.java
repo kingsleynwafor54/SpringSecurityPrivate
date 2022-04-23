148362c80fdbae2d38f02e478b444cff6504ad90
kingsleynwafor54 <61025076+kingsleynwafor54@users.noreply.github.com>
2022-04-23 11:35:57 +0100

    Sprng Security

---
+++ src/main/java/io/springsecurity/userservice/repo/RoleRepo.java
@@ -0,0 +1,8 @@
+package io.springsecurity.userservice.repo;
+
+import io.springsecurity.userservice.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role,Long> {
+    Role findByName(String name);
+}
